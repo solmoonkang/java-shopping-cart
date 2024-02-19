ca879374c0a87e284ca9e6704a38557520290e95
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-02-19 16:07:17 +0900

    init

---
+++ src/CartApp.java
@@ -0,0 +1,17 @@
+public class CartApp {
+    public static void main(String[] args) {
+
+        // TODO: 상품 목록 생성
+
+        // TODO: 상품 클래스를 생성하여 상품 목록에 넣는다.
+
+        // TODO: 상품 목록 확인
+
+        // TODO: 장바구니 생성
+
+
+        // TODO: 상품을 장바구니에 추가
+        // TODO: 상품을 장바구니에서 제거
+        // TODO: 장바구니에 현재 담긴 상품들을 출력 (상품 이름, 각 상품의 개수)
+    }
+}
