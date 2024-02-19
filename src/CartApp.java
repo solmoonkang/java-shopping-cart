363471f95672587490411a4690909c45df36e888
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-02-20 08:29:35 +0900

    feat: Implement CartApp main method for testing product add and show method

--- src/CartApp.java
+++ src/CartApp.java
@@ -1,17 +1,16 @@
+import product.Product;
+import product.Products;
+
 public class CartApp {
     public static void main(String[] args) {
 
-        // TODO: 상품 목록 생성
+        Product milk = new Product(1L, "Milk", 2000);
+        Product bread = new Product(2L, "Bread", 1500);
 
-        // TODO: 상품 클래스를 생성하여 상품 목록에 넣는다.
+        Products products = new Products();
+        products.addProduct(milk);
+        products.addProduct(bread);
 
-        // TODO: 상품 목록 확인
-
-        // TODO: 장바구니 생성
-
-
-        // TODO: 상품을 장바구니에 추가
-        // TODO: 상품을 장바구니에서 제거
-        // TODO: 장바구니에 현재 담긴 상품들을 출력 (상품 이름, 각 상품의 개수)
+        products.showProducts();
     }
 }
