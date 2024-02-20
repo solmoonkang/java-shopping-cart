c94450fba29ae31a546fb94aa1121fb2faa67087
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-02-20 16:04:23 +0900

    feat: Implement CartApp main method for testing

--- src/CartApp.java
+++ src/CartApp.java
@@ -6,10 +6,14 @@ public class CartApp {
 
         Product milk = new Product(1L, "Milk", 2000);
         Product bread = new Product(2L, "Bread", 1500);
+        Product soup = new Product(3L, "Soup", 500);
+        Product water = new Product(4L, "Water", 700);
 
         Products products = new Products();
         products.addProduct(milk);
         products.addProduct(bread);
+        products.addProduct(soup);
+        products.addProduct(water);
 
         products.showProducts();
     }
