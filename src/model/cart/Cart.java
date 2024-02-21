56b6e71feab7dfc1dd68a5a22272cc3e45bdab91
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-02-21 18:31:25 +0900

    refactor: Refactor key values of Map and implement exception handling

--- src/model/cart/Cart.java
+++ src/model/cart/Cart.java
@@ -1,34 +1,52 @@
 package model.cart;
 
+import model.product.Product;
+
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Cart {
 
-    private final Map<String, Integer> cartItems;
+    private final Map<Product, Integer> cartItems;
 
     public Cart() {
         this.cartItems = new HashMap<>();
     }
 
-    public Map<String, Integer> getCartItems() {
+    public Map<Product, Integer> getCartItems() {
         return Collections.unmodifiableMap(cartItems);
     }
 
-    public void addProductToCart(String name, int quantity) {
-        increaseProductQuantityInCart(name, quantity);
+    public void addProductToCart(Product product, int quantity) {
+        increaseProductQuantityInCart(product, quantity);
     }
 
-    public void removeProductToCart(String name, int quantity) {
-        cartItems.remove(name, quantity);
+    public void removeProductToCart(Product product, int quantity) {
+        checkQuantityExceeds(product, quantity);
+        cartItems.put(product, cartItems.get(product) - quantity);
+        removeIfEmpty(product);
     }
 
-    private void increaseProductQuantityInCart(String name, int quantity) {
-        if (cartItems.containsKey(name)) {
-            cartItems.put(name, cartItems.get(name) + quantity);
+    private void increaseProductQuantityInCart(Product product, int quantity) {
+        if (cartItems.containsKey(product)) {
+            cartItems.put(product, cartItems.get(product) + quantity);
         } else {
-            cartItems.put(name, quantity);
+            cartItems.put(product, quantity);
+        }
+    }
+
+    private void checkQuantityExceeds(Product product, int quantity) {
+        int currentQuantity = cartItems.get(product);
+
+        if (currentQuantity == 0 || quantity > currentQuantity) {
+            throw new IllegalArgumentException("[ERROR] 장바구니에 담긴 상품 수량보다 많습니다.");
+        }
+    }
+
+    private void removeIfEmpty(Product product) {
+        if (cartItems.get(product) == 0) {
+            cartItems.remove(product);
         }
     }
 }
