58f98b1040bb435270de9dd15a30d296cea07933
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-02-21 12:07:44 +0900

    feat: Implement parseProduct method

--- src/model/product/Products.java
+++ src/model/product/Products.java
@@ -5,17 +5,29 @@ import java.util.Set;
 
 public class Products {
 
+    private static final String SPACE = " ";
+
     private final Set<Product> products;
 
     public Products() {
         this.products = new HashSet<>();
     }
 
-    public void addProduct(Product product) {
-        products.add(product);
+    public void addProducts(String[] productInputs) {
+        for (String productInput : productInputs) {
+            products.add(parseProduct(productInput));
+        }
     }
 
     public void removeProduct(Product product) {
         products.remove(product);
+    }
+
+    private Product parseProduct(String input) {
+        String[] values = input.split(SPACE);
+        String name = values[0];
+        int price = Integer.parseInt(values[1]);
+
+        return new Product(name, price);
     }
 }
