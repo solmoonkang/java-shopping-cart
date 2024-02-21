8b3dc9451e16b94b8bb738d90d86dd5aafa60561
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-02-21 13:03:51 +0900

    feat: Implement CartController run method

--- src/controller/CartController.java
+++ src/controller/CartController.java
@@ -8,6 +8,7 @@ import java.io.IOException;
 
 public class CartController {
 
+    private static final String COMMA = ", ";
     private final InputView inputView;
 
     public CartController(InputView inputView) {
@@ -17,9 +18,30 @@ public class CartController {
     public void run() throws IOException {
 
         Products products = new Products();
-        String[] productInputs = inputView.inputProduct().split(", ");
-        products.addProducts(productInputs);
+        String userAction = "";
 
+        while (!(userAction = inputView.inputUserAction()).equals("종료")) {
+            userActions(userAction, products);
+        }
+    }
+
+    private void userActions(String userAction, Products products) throws IOException {
+        if (userAction.equals("등록")) {
+            createProduct(products);
+        } else if (userAction.equals("제거")) {
+            removeProduct(products);
+        } else {
+            System.out.println("잘못된 형식입니다. 다시 선택해주세요.");
+        }
+    }
+
+    private void createProduct(Products products) throws IOException {
+        String[] productInputs = inputView.inputAddProduct().split(COMMA);
+        products.addProducts(productInputs);
         OutputView.outputProducts(products);
+    }
+
+    private void removeProduct(Products products) throws IOException {
+        products.removeProduct(inputView.inputRemoveProduct());
     }
 }
