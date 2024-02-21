6d9d88977423c45910aa32a2b0114514c72528d4
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-02-21 10:44:21 +0900

    feat: Implement input product info in InputView class

---
+++ src/view/InputView.java
@@ -0,0 +1,19 @@
+package view;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class InputView {
+
+    private BufferedReader reader;
+
+    public InputView() {
+        this.reader = new BufferedReader(new InputStreamReader(System.in));
+    }
+
+    public String inputProduct() throws IOException {
+        System.out.println("상품 이름과 상품 가격을 입력하세요(예: 상품이름, 가격): ");
+        return reader.readLine();
+    }
+}
