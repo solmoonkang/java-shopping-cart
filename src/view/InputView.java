2835dfebef5318a0322ad8411a5beb51b5ad1ac8
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-02-21 12:35:31 +0900

    feat: Implement inputUserAction method in InputView class

--- src/view/InputView.java
+++ src/view/InputView.java
@@ -16,4 +16,9 @@ public class InputView {
         System.out.println("상품 이름과 상품 가격을 입력하세요(예: 상품이름,가격): ");
         return reader.readLine();
     }
+
+    public String inputUserAction() throws IOException {
+        System.out.println("등록, 제거, 파일 읽기 중 어떤 동작을 수행하시겠습니까?");
+        return reader.readLine();
+    }
 }
