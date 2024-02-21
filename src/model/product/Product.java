85273bc216797ec7ca5b323401e8355558ce58b0
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-02-21 11:04:11 +0900

    feat: Implement Product class

--- src/model/product/Product.java
+++ src/model/product/Product.java
@@ -1,7 +1,6 @@
 package model.product;
 
 import java.util.Objects;
-import java.util.UUID;
 import java.util.concurrent.atomic.AtomicLong;
 
 public class Product {
@@ -16,15 +15,6 @@ public class Product {
         this.id = count.incrementAndGet();
         this.name = name;
         this.price = price;
-    }
-
-    // TODO: 해당 메서드를 Product 클래스에서 처리해줘야 하는지 고민해보자.
-    public String printProductInfo() {
-        return String.format("%s: %d", name, price);
-    }
-
-    public String printCartItemsInfo() {
-        return String.format("%s", name);
     }
 
     @Override
