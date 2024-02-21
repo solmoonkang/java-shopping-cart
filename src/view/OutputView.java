bd23ec3c49708ced27a0197fb225638682cce1be
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-02-21 17:16:49 +0900

    feat: Implement outputCartItems method in OutputView class

--- src/view/OutputView.java
+++ src/view/OutputView.java
@@ -1,27 +1,40 @@
 package view;
 
+import model.cart.Cart;
 import model.product.Product;
 import model.product.Products;
+
+import java.util.Map;
 
 public class OutputView {
 
     public static void outputProducts(Products products) {
-        System.out.println("고유한 상품 목록:");
+        System.out.println("고유한 상품 목록: \n");
 
         for (Product product : products.getProducts()) {
             System.out.println(product.getName() + " : " + product.getPrice());
         }
+        System.out.println();
     }
 
-    public static void outputCartItems() {
-        System.out.println("장바구니에 현재 담긴 상품: ");
+    public static void outputCartItems(Cart cart) {
+        System.out.println("장바구니에 현재 담긴 상품: " + "\n");
+
+        for (Map.Entry<String, Integer> entry : cart.getCartItems().entrySet()) {
+            System.out.println(entry.getKey() + " : " + entry.getValue());
+        }
+        System.out.println();
     }
 
-    public static void outputAddProductToCart(Product product, int quantity) {
-        System.out.println(product.getName() + " " + quantity + "개 담기");
+    public static void outputAddProductToCart(String name, int quantity) {
+        System.out.println(name + " " + quantity + "개 담기");
     }
 
-    public static void outputRemoveProductToCart(Product product, int quantity) {
-        System.out.println(product.getName() + " " + quantity + "개 빼기");
+    public static void outputRemoveProductToCart(String name, int quantity) {
+        System.out.println(name + " " + quantity + "개 빼기");
+    }
+
+    public static void outputInvalidInputError() {
+        System.out.println("잘못된 형식입니다. 다시 입력해주세요.");
     }
 }
