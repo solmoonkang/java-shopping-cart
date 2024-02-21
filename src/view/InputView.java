2e76d9a8a6cf18537856c27e3ed88ed955c5da52
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-02-21 13:04:12 +0900

    feat: Implement inputAddProductToCart method in InputView class

--- src/view/InputView.java
+++ src/view/InputView.java
@@ -12,13 +12,23 @@ public class InputView {
         this.reader = new BufferedReader(new InputStreamReader(System.in));
     }
 
-    public String inputProduct() throws IOException {
-        System.out.println("상품 이름과 상품 가격을 입력하세요(예: 상품이름,가격): ");
+    public String inputUserAction() throws IOException {
+        System.out.println("등록, 제거, 파일 읽기 중 어떤 동작을 수행하시겠습니까?");
         return reader.readLine();
     }
 
-    public String inputUserAction() throws IOException {
-        System.out.println("등록, 제거, 파일 읽기 중 어떤 동작을 수행하시겠습니까?");
+    public String inputAddProduct() throws IOException {
+        System.out.println("추가하고 싶은 상품 이름과 상품 가격을 입력하세요.(예: 상품이름 가격): ");
+        return reader.readLine();
+    }
+
+    public String inputRemoveProduct() throws IOException {
+        System.out.println("제거하고 싶은 상품 이름을 입력하세요.");
+        return reader.readLine();
+    }
+
+    public String inputAddProductToCart() throws IOException {
+        System.out.println("장바구니에 추가하고 싶은 상품 이름과 수량을 입력하세요.(예: 상품 이름 수량): ");
         return reader.readLine();
     }
 }
