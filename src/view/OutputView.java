c2973d99427635594406f79193fc9bc4fb794013
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-02-21 15:20:18 +0900

    feat: Implement OutputView class

--- src/view/OutputView.java
+++ src/view/OutputView.java
@@ -7,8 +7,21 @@ public class OutputView {
 
     public static void outputProducts(Products products) {
         System.out.println("고유한 상품 목록:");
+
         for (Product product : products.getProducts()) {
             System.out.println(product.getName() + " : " + product.getPrice());
         }
+    }
+
+    public static void outputCartItems() {
+        System.out.println("장바구니에 현재 담긴 상품: ");
+    }
+
+    public static void outputAddProductToCart(Product product, int quantity) {
+        System.out.println(product.getName() + " " + quantity + "개 담기");
+    }
+
+    public static void outputRemoveProductToCart(Product product, int quantity) {
+        System.out.println(product.getName() + " " + quantity + "개 빼기");
     }
 }
