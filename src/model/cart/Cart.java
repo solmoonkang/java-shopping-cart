7458ac9e255e56f81e07386aaaf97ce8f17dcba9
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-02-21 17:18:06 +0900

    feat: Implement add and remove product to cart methods in Cart class

--- src/model/cart/Cart.java
+++ src/model/cart/Cart.java
@@ -1,61 +1,34 @@
 package model.cart;
 
-import model.product.Product;
-
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Cart {
 
-    private final Map<Product, Integer> cartItems;
+    private final Map<String, Integer> cartItems;
 
     public Cart() {
         this.cartItems = new HashMap<>();
     }
 
-    public void addProductToCart(Product product, int quantity) {
-        increaseProductQuantityInCart(product, quantity);
+    public Map<String, Integer> getCartItems() {
+        return Collections.unmodifiableMap(cartItems);
     }
 
-    public void removeProductToCart(Product product, int quantity) {
-        removeOrUpdateProductToCart(product, quantity);
+    public void addProductToCart(String name, int quantity) {
+        increaseProductQuantityInCart(name, quantity);
     }
 
-    // TODO: 제거하고 싶은 상품의 수량이 장바구니에 있는 수량보다 많을 경우, "장바구니에 상품이 충분하지 않다"는 메시지와 함께 다시 입력받도록 구현
-    private void removeOrUpdateProductToCart(Product product, int quantity) {
-        if (isRemovableQuantity(product, quantity)) {
-            updateProduct(product, quantity);
+    public void removeProductToCart(String name, int quantity) {
+        cartItems.remove(name, quantity);
+    }
+
+    private void increaseProductQuantityInCart(String name, int quantity) {
+        if (cartItems.containsKey(name)) {
+            cartItems.put(name, cartItems.get(name) + quantity);
         } else {
-            System.out.println("장바구니에 상품이 충분하지 않습니다.");
+            cartItems.put(name, quantity);
         }
-    }
-
-    private boolean isRemovableQuantity(Product product, int quantity) {
-        int currentQuantity = cartItems.getOrDefault(product, 0);
-        return currentQuantity >= quantity;
-    }
-
-    private void updateProduct(Product product, int quantity) {
-        int currentQuantity = cartItems.get(product);
-        int newQuantity = currentQuantity - quantity;
-
-        if (newQuantity == 0) {
-            cartItems.remove(product);
-        } else {
-            cartItems.put(product, newQuantity);
-        }
-    }
-
-    private void increaseProductQuantityInCart(Product product, int quantity) {
-        if (cartItems.containsKey(product)) {
-            cartItems.put(product, cartItems.get(product) + quantity);
-        } else {
-            cartItems.put(product, quantity);
-        }
-    }
-
-    // TODO: 기존에 등록된 상품이 있을 경우에만 장바구니에 상품을 추가할 수 있도록 검증하는 메서드
-    private void validateProductExistence(Product product) {
-
     }
 }
