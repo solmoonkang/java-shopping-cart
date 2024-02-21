0bfea7249aa85f4ff31f104fbc5421c31924e738
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-02-21 12:21:03 +0900

    feat: Implement main method for testing

--- src/CartApp.java
+++ src/CartApp.java
@@ -1,28 +1,12 @@
-import cart.Cart;
-import product.Product;
-import product.Products;
+import controller.CartController;
+import view.InputView;
+
+import java.io.IOException;
 
 public class CartApp {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
-        Product milk = new Product(1L, "Milk", 2000);
-        Product bread = new Product(2L, "Bread", 1500);
-        Product tissue = new Product(3L, "Tissue", 1200);
-        Product water = new Product(4L, "Water", 700);
-
-        Products products = new Products();
-        products.addProduct(milk);
-        products.addProduct(bread);
-        products.addProduct(tissue);
-        products.addProduct(water);
-
-        products.removeProduct(water);
-
-        products.showProduct();
-
-        Cart cart = new Cart();
-        cart.addProductToCart(milk, 2);
-        cart.addProductToCart(milk, 4);
-        cart.showCartItems();
+        CartController cartController = new CartController(new InputView());
+        cartController.run();
     }
 }
